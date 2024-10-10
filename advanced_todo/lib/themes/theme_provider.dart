f31c6087c9065ba5c6204d4ad864578cad46a9c1
nkusikevin <nkusikvnhart@gmail.com>
2024-10-10 19:18:10 +0200

    adding persisted theme storage

--- advanced_todo/lib/themes/theme_provider.dart
+++ advanced_todo/lib/themes/theme_provider.dart
@@ -2,16 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:advanced_todo/themes/dark_mode.dart';
 import 'package:advanced_todo/themes/light_mode.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final themeProvider = StateNotifierProvider<ThemeNotifier, ThemeData>((ref) {
   return ThemeNotifier();
 });
 
 class ThemeNotifier extends StateNotifier<ThemeData> {
-  ThemeNotifier() : super(lightMode);
+  ThemeNotifier() : super(lightMode) {
+    _loadTheme();
+  }
 
-  void toggleTheme() {
-    state = state == lightMode ? darkMode : lightMode;
+  static const String _themeKey = 'is_dark_mode';
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDarkMode = prefs.getBool(_themeKey) ?? false;
+    state = isDarkMode ? darkMode : lightMode;
+  }
+
+  Future<void> _saveTheme(bool isDarkMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themeKey, isDarkMode);
+  }
+
+  Future<void> toggleTheme() async {
+    final newTheme = state == lightMode ? darkMode : lightMode;
+    state = newTheme;
+    await _saveTheme(isDarkMode);
   }
 
   bool get isDarkMode => state == darkMode;
