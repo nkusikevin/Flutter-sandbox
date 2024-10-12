8939c4894efbe841a4d43d25b2a9e5003c35f9b2
nkusikevin <nkusikvnhart@gmail.com>
2024-10-12 21:24:53 +0200

    adding online checker

--- auth/lib/main.dart
+++ auth/lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 
 //frebase related imports
 import 'package:firebase_core/firebase_core.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'firebase_options.dart';
+import 'package:auth/components/snackBar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,10 +15,35 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isFirstTime = true;
+
+  @override
+  void initState() {
+    super.initState();
+    InternetConnectionChecker().onStatusChange.listen((status) {
+      switch (status) {
+        case InternetConnectionStatus.connected:
+          if (isFirstTime) {
+            isFirstTime = false;
+            break;
+          }
+          print('Connected');
+          break;
+        case InternetConnectionStatus.disconnected:
+          print('Disconnected');
+          break;
+      }
+    });
+  }
+
   Widget build(BuildContext context) {
     return const MaterialApp(
         title: 'Authentify',
