677961b27105384475bcc456b6c8b1d49e8262de
nkusikevin <nkusikvnhart@gmail.com>
2024-10-14 10:55:07 +0200

    adding onchnage track

--- blockie/blockie/lib/counter/counter_cubit.dart
+++ blockie/blockie/lib/counter/counter_cubit.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<int> {
-  CounterCubit(super.initialState) ;
+  CounterCubit(super.initialState);
 
   void increment() => emit(state + 1);
   void decrement() => emit(state - 1);
+  @override
+  void onChange(Change<int> change) {
+    print(change);
+    super.onChange(change);
+  }
 }
